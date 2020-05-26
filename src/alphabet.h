769b407619323b293f540dd8d7d2fdc65e746244
sergeybayandin <serezha-bayandin@mail.ru>
2020-05-27 01:41:46 +0300

    Implementing an initial Alphabet class prototype

---
+++ src/alphabet.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <fstream>
+
+class Alphabet
+{
+	std::ifstream inputText;
+	std::ofstream outputText;
+	unsigned char *buffer;
+	char sym;
+	size_t lengthStrMax, iBuff, iWork, lengthWord;
+public:
+	Alphabet(const char* name);
+	~Alphabet();
+	
+	/*
+	*
+	*        Methods 
+	*
+	*/
+}
