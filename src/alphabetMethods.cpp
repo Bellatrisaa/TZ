1d2efcba34ee4a1b09563f6951d5b0380da14343
Bellatrisa <ignatiev_sea@mail.ru>
2020-05-27 02:02:16 +0700

    ADD alphabetMethods

---
+++ src/alphabetMethods.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include "singleList.h"
+#include <string>
+#include "tools.h"
+#include "alphabet.h"
+
+Alphabet::Alphabet(const char *name)
+{
+	inputText.open(name);
+	buffer = NULL;
+	lengthStrMax = 0;
+	iBuff = 0;
+	lengthWord = 0;
+	iWork = 0;
+}
+
+Alphabet::~Alphabet()
+{
+	delete[] buffer;
+}
+
+void Alphabet::openisfile()
+{
+	if (inputText.is_open())
+	{
+		std::cout << "FILE IS OPEN" << std::endl;
+	}
+	else
+	{
+		std::cout << "FILE IS NOT OPEN" << std::endl;
+	}
+}
+
+void Alphabet::printfile()
+{
+	while (inputText.get(sym))
+	{
+		std::cout << sym;
+	}
+}
+
+void Alphabet::filetoarray()
+{
+	countsymbols(inputText, lengthStrMax);
+	buffer = new unsigned char[lengthStrMax];
+	while (inputText.get((char&)sym) and iBuff < lengthStrMax)
+	{
+		buffer[iBuff] = sym;
+		iBuff++;
+	}
+	iBuff = 0;
+	returntostart(inputText);
+	inputText.close();
+}
+
+void Alphabet::printarray()
+{
+	if (buffer != NULL)
+	{
+		for (iBuff = 0; iBuff < lengthStrMax; iBuff++)
+		{
+			std::cout << buffer[iBuff];
+		}
+	}
+	else
+	{
+		std::cout << "ERROR ARRAY IS ENPTY\n";
+	}
+}
+
+void Alphabet::deletepuncmark()
+{
+	int k = 0;
+	if (buffer != NULL)
+	{
+		for (iBuff = 0; iBuff < lengthStrMax; iBuff++)
+		{
+			if (IsItLetter(buffer[iBuff]))
+			{
+				iWork = iBuff;
+				iBuff = countsymbols(lengthWord, buffer, iWork, lengthStrMax);
+				lwords.push(lengthWord, buffer, iWork, iBuff);
+				k++;
+				lengthWord = 0;
+			}
+		}
+		std::cout << lengthStrMax << std::endl;
+	}
+	else
+	{
+		std::cout << "Massive is empty\n";
+	}
+}
+
+void Alphabet::tolower_()
+{
+	lwords.tolower_();
+}
+
+void Alphabet::sort()
+{
+	lwords.sort_alphabet();
+}
+
+void Alphabet::show()
+{
+	lwords.printList();
+}
+
+void Alphabet::tofile()
+{
+	lwords.listtofile();
+}
