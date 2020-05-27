95701ba22e82feb6883d8ba5e9c3d4305d9738d6
sergeybayandin <serezha-bayandin@mail.ru>
2020-05-27 19:45:46 +0300

    Add destructor for class list

--- src/singleList.cpp
+++ src/singleList.cpp
@@ -14,3 +14,19 @@ list::list()
 {
 	head = NULL;  
 }
+
+list::~list()
+{
+	Node* ptr = head;
+	while (ptr != NULL)
+	{
+		delete[] ptr->word;
+		ptr = ptr->next;         
+	}
+	while (head != NULL)
+	{
+		Node* temp = head;
+		head = head->next;
+		delete[] temp;
+	}
+}
