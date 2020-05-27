81287a245d1d9dfeecfd787f9fd0499cf02f3c6e
sergeybayandin <serezha-bayandin@mail.ru>
2020-05-27 18:44:29 +0300

    Add prototype: countsymbols()

--- src/tools.h
+++ src/tools.h
@@ -3,4 +3,4 @@
 void countsymbols(std::ifstream& file, size_t& len);
 void returntostart(std::ifstream& file);
 bool IsItLetter(unsigned char& sym);
-
+size_t countsymbols(size_t& len, unsigned char* sym, size_t i, size_t lengthStrMax);
