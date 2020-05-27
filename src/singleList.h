c05866fabd057f2ee3846580c1a511596ccaeffc
Bellatrisa <ignatiev_sea@mail.ru>
2020-05-27 19:02:03 +0700

    Add signalList.h

---
+++ src/singleList.h
@@ -0,0 +1,22 @@
+#pragma once
+
+/*SingleList*/
+
+class list
+{
+	struct Node;
+
+public:
+	list();
+	~list();
+
+	void push(size_t length, unsigned char* array, size_t start, size_t finish);
+	void tolower_();
+	void sort_alphabet();
+	void printList();
+	void listtofile();
+
+private:
+	Node* pred; /*”казатель на предыдущий элемент*/
+	Node* head; /*”казатель на начало списка*/
+};
