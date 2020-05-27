4f7af49adf6719c316d81e6dd6e23bc78687f70a
sergeybayandin <serezha-bayandin@mail.ru>
2020-05-27 22:31:16 +0300

    Add object of class Alphabet

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "alphabet.h"
+#include <locale.h>
+
+int main(int argv, char** argc)
+{
+	setlocale(LC_ALL, "RUS");
+	Alphabet text("input.txt");
+	text.openisfile();
+	text.filetoarray();
+	text.deletepuncmark();
+	text.tolower_();
+	text.sort();
+	text.show();
+	text.tofile();
+	return 0;
+}
