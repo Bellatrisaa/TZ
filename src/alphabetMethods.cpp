22478c45a1433e474edef4d82a84beb588790575
sergeybayandin <serezha-bayandin@mail.ru>
2020-05-27 12:16:30 +0300

    Add method filetoarray

--- src/alphabetMethods.cpp
+++ src/alphabetMethods.cpp
@@ -5,26 +5,14 @@ Alphabet::Alphabet(const char *name)
 	inputText.open(name);
 	buffer = NULL;
 	lengthStrMax = 0;
-	iBuff = 0;
+	iBuff = 0;                       
 	lengthWord = 0;
 	iWork = 0;
 }
 
 Alphabet::~Alphabet()
 {
-	delete[] buffer;
-}
-
-void Alphabet::openisfile()
-{
-	if (inputText.is_open())
-	{
-		std::cout << "FILE IS OPEN" << std::endl;
-	}
-	else
-	{
-		std::cout << "FILE IS NOT OPEN" << std::endl;
-	}
+	delete[] buffer;                
 }
 
 void Alphabet::openisfile()
@@ -45,4 +33,18 @@ void Alphabet::printfile()
 	{
 		std::cout << sym;
 	}
+}
+
+void Alphabet::filetoarray()
+{
+	countsymbols(inputText, lengthStrMax);
+	buffer = new unsigned char[lengthStrMax];
+	while (inputText.get((char&)sym) and iBuff < lengthStrMax)
+	{
+		buffer[iBuff] = sym;
+		iBuff++;
+	}
+	iBuff = 0;
+	returntostart(inputText);
+	inputText.close();
 }
