0c2076c36e7221961dcc6cffe5187f2b67bf349b
sergeybayandin <serezha-bayandin@mail.ru>
2020-05-27 19:52:45 +0300

    Add method push() for class list

--- src/singleList.cpp
+++ src/singleList.cpp
@@ -30,3 +30,37 @@ list::~list()
 		delete[] temp;
 	}
 }
+
+void list::push(size_t length, unsigned char* array, size_t start, size_t finish)
+{
+	if (head == NULL)
+	{
+		head = new Node;
+		head->word = new unsigned char[length];
+		head->length = length;
+		size_t i = 0;
+		for (start; start < finish; start++)
+		{
+			head->word[i] = array[start];
+			i++;
+		}                                             
+		head->next = NULL;                            
+		pred = head;                                  
+	}                                                 
+	else                                              
+	{
+		Node* ptr = new Node;
+
+		ptr->word = new unsigned char[length];
+		ptr->length = length;
+		size_t i = 0;
+		for (start; start < finish; start++)
+		{
+			ptr->word[i] = array[start];
+			i++;
+		}
+		pred->next = ptr;
+		pred = ptr;
+		pred->next = NULL;
+	}
+}
