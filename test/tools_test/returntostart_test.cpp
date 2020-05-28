cc2b4d471039b430b82120151f88215a1e0d83fb
sergeybayandin <serezha-bayandin@mail.ru>
2020-05-28 03:40:02 +0300

    Add tests for function returntostart()

---
+++ test/tools_test/returntostart_test.cpp
@@ -0,0 +1,13 @@
+#include <catch.hpp>
+#include <fstream>
+#include "tools.h"
+#include <iostream>
+
+TEST_CASE("Test function returntostart", "File: returntostart_test.cpp")
+{
+	std::ifstream test_1_txt;
+	test_1_txt.open("test/tools_test/test_txt_files/test_1.txt");
+	test_1_txt.seekg(5);
+	returntostart(test_1_txt);
+	REQUIRE(test_1_txt.tellg() == 0);
+}
