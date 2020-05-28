a36eab7924b97ed634784cb1ec810a87b963e0ed
sergeybayandin <serezha-bayandin@mail.ru>
2020-05-28 16:53:00 +0300

    Fix test/tools_test/countsymbols.cpp

--- test/tools_test/countsymbols_test.cpp
+++ test/tools_test/countsymbols_test.cpp
@@ -14,7 +14,7 @@ TEST_CASE("Character count test", "File: countsymbols_test.cpp")
 
 TEST_CASE("Character count test (txt file is empty)", "File: countsymbols_test.cpp")
 {
-	std::ifstream test_2_1_txt;
+	std::ifstream test_2_2_txt;
 	test_2_2_txt.open("test/tools_test/test_txt_files/test_2.2.txt");
 	size_t length = 0;
 	countsymbols(test_2_2_txt, length);
