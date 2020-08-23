194c49ab65d3d751b72402353eafca0f0534f5c5
Bellatrisa <ignatiev_sea@mail.ru>
2020-08-24 01:52:17 +0700

    Fixed data entry system

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
 #include "alphabet.h"
 #include <locale.h>
+#include <string>
+#include <iostream>
+using namespace std;  
 
 int main(int argv, char** argc)
 {
     setlocale(LC_ALL, "RUS");
-    alphabet text("input.txt");
+    string sf; 
+	printf("\n      ¬ведите название txt файла с исходными данными: ");  	
+	getline(cin,sf);
+	sf=sf+".txt";
+    alphabet text(sf.c_str());
     text.openisfile();
     text.filetoarray();
     text.deletepuncmark();
@@ -12,5 +19,6 @@ int main(int argv, char** argc)
     text.sort();
     text.show();
     text.tofile("output.txt");
+    system("pause");
     return 0;
 }
