bb675e0ce104723138bee529eebe29bf1ff93b9c
Bellatrisa <ignatiev_sea@mail.ru>
2020-08-29 23:35:24 +0700

    Added global_test

---
+++ test/global_test.cpp
@@ -0,0 +1,37 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "Alphabet.h"
+#include <string.h>
+#include <fstream>
+#include <iostream>
+
+TEST_CASE("[main]", "Main Function")
+{
+
+    setlocale(LC_ALL, "RUS");
+
+    Alphabet alph("test_input.txt");
+
+    alph.toFile();
+   
+	bool result = true;
+    char ch_1[30], ch_2[30];
+
+    FILE* file_1;
+    FILE* file_2;
+
+    file_1 = fopen("test_output.txt", "r");
+    file_2 = fopen("output.txt", "r");
+        
+    while (!feof(file_1) && !feof(file_2)) {
+          fgets(ch_1, 30, file_1);
+          fgets(ch_2, 30, file_2);
+          if (strcmp(ch_1, ch_2) != 0) {
+              result = false;
+          }
+    } 
+
+     fclose(file_1);
+     fclose(file_2);
+     REQUIRE(result == true);
+    }
